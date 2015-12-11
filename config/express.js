8fe842be657a01879c14d84b1da6193faabaa79e
Stephann Vasconcelos <stephannvasconcelos@gmail.com>
2015-12-11 14:19:58 -0300

    Primeiro commit. Configurando routes no Express

---
+++ config/express.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var home = require( '../app/routes/home');
+
+module.exports = function(){
+	var app = express();
+	
+	//configuração de ambiente
+	app.set('port', 3000);
+
+	//middleware
+	app.use( express.static('./public') );
+
+	app.set( 'view engine', 'ejs' );
+	app.set(  'views', './app/views' );
+
+	home( app );
+	
+	return app;	
+}
