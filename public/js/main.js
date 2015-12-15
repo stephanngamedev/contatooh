c96cbd36947fe303d46d042511aaccde9ffe3825
Stephann Vasconcelos <stephannvasconcelos@gmail.com>
2015-12-15 15:30:04 -0300

    Criando as rotas do ngRoute

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
 angular.module( 'contatooh', [ 'ngRoute' ] )
+
+.config( function( $routeProvider ) {
+	$routeProvider.when('/contatos' , {
+		templateUrl: 'partials/contatos.html',
+		controller: 'ContatosController'
+	});
+
+	$routeProvider.when('/contato/:contatoId', {
+		templateUrl: 'partials/contato.html',
+		controller: 'ContatosController'
+	});
+});
