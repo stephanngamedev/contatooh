cfcaf7edc7d4de60347dadef1bc7d47c59aa4a41
Stephann Vasconcelos <stephannvasconcelos@gmail.com>
2015-12-15 20:24:04 -0300

    Utilizando o ngResource para retornar os contatos

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -2,15 +2,19 @@ angular.module( 'contatooh' ).controller( 'ContatosController',
 	function( $resource, $scope ) {
 		$scope.contatos = [];
 	
-		$scope.filtro = ''
+		$scope.filtro = '';
 
-		$http.get('/contatos')
-			.success( function( data ) {
-				$scope.contatos = data;
-			})
-			.error( function( statusText ) {
-				console.log( 'Não foi possível obter a lista de contatos' );
-				console.log( statusText );
-			});
+		var Contato = $resource('/contatos/:id');
+
+		function buscaContatos() {
+			Contato.query(
+				function( contatos ) {
+					$scope.contatos = contatos;
+				},
+				function( error ) {
+					console.log( error );
+				}
+			);	
+		};
 	}
 );
