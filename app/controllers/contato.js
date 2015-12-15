aaf873389f00f8e50c0d4a980032a4fce180fb98
Stephann Vasconcelos <stephannvasconcelos@gmail.com>
2015-12-15 20:06:36 -0300

    Usando o  para fazer a conexão com o servidor

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -2,7 +2,7 @@ module.exports = function() {
 	var controller = {}
 
 	var contatos = [
-		{_id: 1, nome: 'Contato Exemplo 1', email: 'cont1@empresa.com.br'},
+		{_id: 1, nome: 'Contato Exemplo A', email: 'cont1@empresa.com.br'},
 		{_id: 2, nome: 'Contato Exemplo 2', email: 'cont2@empresa.com.br'},
 		{_id: 3, nome: 'Contato Exemplo 3', email: 'cont3@empresa.com.br'}
 	];
--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,26 +1,16 @@
-angular.module( 'contatooh' ).controller( 'ContatosController', function( $scope ) {
-	$scope.total = 0;
+angular.module( 'contatooh' ).controller( 'ContatosController', 
+	function( $http, $scope ) {
+		$scope.contatos = [];
+	
+		$scope.filtro = ''
 
-	$scope.incrementa = function() {
-		$scope.total++;
-	};
-
-	$scope.filtro = ''
-	$scope.contatos = [
-		{
-			"_id": 1,
-			"nome": "Contato Angular 1",
-			"email": "cont1@empresa.com.br"
-		},
-		{
-			"_id": 2,
-			"nome": "Contato Angular 2",
-			"email": "cont2@empresa.com.br"
-		},
-		{
-			"_id": 3,
-			"nome": "Contato Angular 3",
-			"email": "cont3@empresa.com.br"
-		},
-	];
-});
+		$http.get('/contatos')
+			.success( function( data ) {
+				$scope.contatos = data;
+			})
+			.error( function( statusText ) {
+				console.log( 'Não foi possível obter a lista de contatos' );
+				console.log( statusText );
+			});
+	}
+);
