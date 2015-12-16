9c8a89bb59ce8408eb641b9ba8b18bdc355a324a
Stephann Vasconcelos <stephannvasconcelos@gmail.com>
2015-12-16 14:41:13 -0300

    Implementando tela de detalhes do contato

--- public/js/controllers/ContatoController.js
+++ public/js/controllers/ContatoController.js
@@ -1,3 +1,13 @@
-angular.module( 'contatooh' ).controller( 'ContatoController', function( $scope, $routeParams ) {
-	console.log( $routeParams.contatoId );
+angular.module( 'contatooh' ).controller( 'ContatoController', function( $scope, $routeParams, $resource ) {
+	var Contato = $resource('/contatos/:id');
+
+	Contato.get({ id: $routeParams.contatoId },
+		function( contato ) {
+			$scope.contato = contato;
+		},
+		function( error ) {
+			console.log( error );
+			$scope.mensagem = { texto: 'Não foi possível listar o contato.'};
+		}
+	);
 })
