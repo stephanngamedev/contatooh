894cd17e108cb039ac28ee72055af8f067e2e1bf
Stephann Vasconcelos <stephannvasconcelos@gmail.com>
2015-12-17 14:35:25 -0300

    Configurando passport e login com github

--- config/express.js
+++ config/express.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var load = require("express-load");
 var bodyParser = require("body-parser");
-
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport')
 module.exports = function(){
 	var app = express();
 	
@@ -18,6 +20,16 @@ module.exports = function(){
 	app.use( bodyParser.json() );
 
 	app.use( require('method-override')() );
+
+	app.use( cookieParser() );
+	app.use( session({
+		secret: 'hello',
+		resave: true,
+		saveUninitialized: true
+	}));
+
+	app.use( passport.initialize() );
+	app.use( passport.session() );
 
 	load( 'models', { cwd: 'app'} )
 		.then('controllers')
